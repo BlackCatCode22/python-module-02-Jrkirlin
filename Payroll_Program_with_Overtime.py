13b24e3af9e9be1609f2274de8dab4332466a693
Jrkirlin <0848278@my.scccd.edu>
2024-09-06 14:32:54 -0700

    Add files via upload
    I fixed it again after watching more videos, but I didn't make separate files for the two programs.

--- Payroll_Program_with_Overtime.py
+++ Payroll_Program_with_Overtime.py
@@ -2,20 +2,20 @@
 hours_weekly = input("Enter hours: ")
 pay_rate = input("Enter rate: ")
 try:
-    hour_check = int(hours_weekly)
-    pay_check = int(pay_rate)
-    no_ot_net_pay = float(hours_weekly) * float(pay_rate)
-    ot_pay_rate = float(pay_rate) * 1.5
-    ot_hours = float(hours_weekly) - 40
-    ot_pay = float(ot_hours) * float(ot_pay_rate)
-    ot_net_pay = float(ot_pay) + (float(pay_rate) * 40)
-
-
-    if float(hours_weekly) <= 40:
-        print("Gross pay :", no_ot_net_pay)
-
-    else:
-        print("Gross pay with overtime:", ot_net_pay)
-
+    hour_check = float(hours_weekly)
+    pay_check = float(pay_rate)
 except:
     print("Please only enter integer values.")
+    quit()
+no_ot_net_pay = float(hours_weekly) * float(pay_rate)
+ot_pay_rate = float(pay_rate) * 1.5
+ot_hours = float(hours_weekly) - 40
+ot_pay = float(ot_hours) * float(ot_pay_rate)
+ot_net_pay = float(ot_pay) + (float(pay_rate) * 40)
+
+
+if float(hours_weekly) <= 40:
+    print("Gross pay :", no_ot_net_pay)
+
+else:
+    print("Gross pay with overtime:", ot_net_pay)
